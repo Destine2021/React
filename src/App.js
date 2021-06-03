312f56cd2c90b172ea62acd27476b329bf4e29eb
Destine <destine.jocet@gmail.com>
2021-06-03 09:34:29 +0200

    elsy day2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Box from "./components/Box";
+import "./styles/global.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const tempMin = -20
+const tempMax = 40
+const heartMin = 80
+const heartMax = 180
+const stepMin = 0
+const stepMax = 50000
+class App extends React.Component {
+
+  constructor() {
+    super();
+
+    this.state = {  // Ne pas oublier "this" !
+      water: 0,
+      heart: 120,
+      temperature: -10,
+      steps: 3000,
+
+    };
+  };
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+
+          {/*<water/>*/}
+          <Box icon="local_drink" color="#3A85FF" value={1.5} unit="L" />
+          
+          {/*<Steps/>*/}
+          <Box icon="directions_walk" color="black" value={3000} unit="steps" />
+          {/*<Heart/>*/}
+          <Box icon="favorite" color="red" value={120} unit="bmp" />
+          {/*<Heart/>*/}
+          <Box icon="wb_sunny" color="yellow" value={-10} unit="°C" />
+
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Box.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+class Box extends React.Component {
+
+  render() {
+    let slider = null;
+    if (this.props.icon != 'local_drink') {
+      slider = (
+        <input type="range" id="volume" name="jauge" min="0" max="100"></input>
+      );
+    }
+    return (
+      <div className="box col-sm-3 col-6">
+        <span
+          className="material-icons "
+          style={{fontSize: 100, color: this.props.color}}
+        >
+          {this.props.icon}
+        </span>
+        <p>
+          {this.props.value}
+          <br />
+          {this.props.unit}
+        </p>
+        {slider}
+      </div>
+    );
+  }
+}
+export default Box;
+        
